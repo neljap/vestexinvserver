795f132369daadffe95c7808c09da73fa5700b84
Ubong Nkantah <ubongnkantahuwem@gmail.com>
2025-09-05 21:59:41 +0100

    update

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(
+  express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 })
+);
 
 const connectMongoDB = require('./config/mongodbConfig');
 const userRoutes = require('./routes/userRoutes');
